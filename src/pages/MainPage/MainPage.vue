01039f4da05cc0c049fcaaa0768f2890309d78ad
Сафонтьев Иван <safontev@bk.ru>
2026-09-02 23:14:02 +0500

    fix

--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -8,11 +8,11 @@ import AddictiveSearchBlock from "./blocks/AddictiveSearchBlock.vue";
 </script>
 <template>
 	<main class="flex flex-col gap-16">
-		<CarouselBlock></CarouselBlock>
-		<LatestOffers></LatestOffers>
-		<MostSelled></MostSelled>
-		<BestCategories></BestCategories>
-		<IdeasBlock></IdeasBlock>
-		<AddictiveSearchBlock></AddictiveSearchBlock>
+		<CarouselBlock/>
+		<LatestOffers/>
+		<MostSelled/>
+		<BestCategories/>
+		<IdeasBlock/>
+		<AddictiveSearchBlock/>
 	</main>
 </template>
